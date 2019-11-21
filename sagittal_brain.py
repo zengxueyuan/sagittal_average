7c55d9c10115544bc4aaea6da0b707c24d37f1e2
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2019-11-21 15:05:33 +0000

    Fixes the ugly bug that was making that our restults were wrong

--- sagittal_brain.py
+++ sagittal_brain.py
@@ -16,7 +16,7 @@ def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv')
 
     # Calculates the averages through the sagittal/horizontal planes
     # and makes it as a row vector
-    averages = planes.mean(axis=0)[np.newaxis, :]
+    averages = planes.mean(axis=1)[np.newaxis, :]
 
     # write it out on my file
     np.savetxt(file_output, averages, fmt='%.1f', delimiter=',')
