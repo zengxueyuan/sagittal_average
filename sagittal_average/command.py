nuttamas/rsd-sagital_average

Moves the executing script as a shell command accessible from anywhere

sagital_average/command.py
@@ -1,0 +1,14 @@
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+from .sagital_brain import run_averages
+
+def process():
+    parser = ArgumentParser(description="Calculates the average for each sagital-horizontal plane.",
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('file_input', nargs='?', default="brain_sample.csv",
+                        help="Input CSV file with the results from scikit-brain binning algorithm.")
+    parser.add_argument('--file_output', '-o', default="brain_average.csv",
+                        help="Name of the output CSV file.")
+    arguments = parser.parse_args()
+
+    run_averages(arguments.file_input, arguments.file_output)

zengxueyuan/sagittal_average

sagittal_average/command.py
@@ -1,0 +1,14 @@
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+from .sagittal_brain import run_averages
+
+def process():
+    parser = ArgumentParser(description="Calculates the average for each sagittal-horizontal plane.",
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('file_input', nargs='?', default="brain_sample.csv",
+                        help="Input CSV file with the results from scikit-brain binning algorithm.")
+    parser.add_argument('--file_output', '-o', default="brain_average.csv",
+                        help="Name of the output CSV file.")
+    arguments = parser.parse_args()
+
+    run_averages(arguments.file_input, arguments.file_output)